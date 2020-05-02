4ae97b5c85f7c25b1aa2492f1956c1cbc5e6a078
Andre Hoong <hoongandre@gmail.com>
2020-05-02 01:43:32 -0700

    Lab01 - console functions read / write

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your username: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please enter your password: ");
+            string password = Console.ReadLine();
+            if (password == "ma5_t3r")
+            {
+                Console.WriteLine("Hello " + name + ", and welcome.");
+            }
+            else
+            {
+                Console.Write("Password not recognized");
+            }
+            Console.ReadKey();
+        }
+    }
+}
